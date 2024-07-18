b2c43445c0860f21ede01ff1b5c958eb80700ebe
harjeevansingh <harjeevansingh01@gmail.com>
2024-07-18 19:14:58 +0530

    time and pointers

---
+++ 07mypointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Welcome to pointers in golang")
+
+	// var ptr *int
+	// fmt.Println("Value of pointer is: ", ptr) 
+
+	myNumber := 23
+
+	var ptr = &myNumber
+	fmt.Println("Value of actual pointer is: ", ptr)
+	fmt.Println("Value of inside the pointer is: ", *ptr)
+
+	*ptr = *ptr * 2
+	fmt.Println("Value of myNumber is: ", myNumber)
+}
