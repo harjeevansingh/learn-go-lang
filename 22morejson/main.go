04ebc29627f86ef118ba71f5527e83e106726db8
harjeevansingh <harjeevansingh01@gmail.com>
2024-07-25 22:32:08 +0530

    more json

--- 22morejson/main.go
+++ 22morejson/main.go
@@ -16,7 +16,8 @@ type course struct {
 func main() {
 	fmt.Println("Welcome to json")
 
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -33,4 +34,42 @@ func EncodeJson() {
 	}
 
 	fmt.Println("Json data is: ", string(finalJson))
+}
+
+func DecodeJson()  {
+	fmt.Println("Decoding json")
+	jsonDataFromWeb := []byte(`
+	 {
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"Platform": "LearnCodeOnline.in",
+		"Password": "abc123",
+		"Tags": [
+			"web-dev",
+			"js",
+			"react"
+		]
+	}
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("Json is valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("Course is: %#v\n", lcoCourse)
+	} else {
+		fmt.Println("Json is invalid")
+	}
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("Course is: %#v\n", myOnlineData)
+
+	for key, value := range myOnlineData {
+		fmt.Printf("Key is: %v \t and value is: %v \t and type is: %T\n", key, value, value)
+	}
+
+
 }
