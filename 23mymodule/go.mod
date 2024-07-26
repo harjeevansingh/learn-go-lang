161d920c3b544ed84d497b4c2d60b7e9dff91205
harjeevansingh <harjeevansingh01@gmail.com>
2024-07-26 13:03:47 +0530

    modules (mod command)

---
+++ 23mymodule/go.mod
@@ -0,0 +1,5 @@
+module github.com/harjeevansingh/golang/23mymodule
+
+go 1.22.5
+
+require github.com/gorilla/mux v1.8.1
---
+++ 23mymodule/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main()  {
+	fmt.Println("Welcome to my module")
+	greeter()
+
+	r := mux.NewRouter()
+    r.HandleFunc("/", serveHome).Methods("GET")
+
+	// http.ListenAndServe(":4000", r)
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+	
+}
+
+func greeter()  {
+	fmt.Println("Hello there!")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Welcome to the home page")
+	w.Write([]byte("Welcome to the home page"))
+}
